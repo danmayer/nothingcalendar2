3191946ad3236d25609ddcc5306f4e6f7bc95ff0
Dan Mayer <dan@mayerdan.com>
2013-05-18 11:39:08 -0400

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Nothingcalendar2::Application.config.secret_token = '59fa3537cc635d8c54c883152cf2a00b3a44319df7558280785f607170ddde3f2409998ce51b1ecb7496fff0ecb8088ad65a4ef6be06c7413c07a6efd84dad7b'
+Nothingcalendar2::Application.config.secret_token = 'd13f3934bdcb15c6e7fdd20e8aca3c3bcc8f1a04de9a3a6a40cf0ba95cd839875d6f00e5a26c39c09b2b03a112b42c2b39bdd6dd191cb5aa2e6d31bf00be5bff'
